39ee7ff11a571ba209f622057da3a7c7a3dca292
HELLO <geraldywy@gmail.com>
2023-04-08 18:22:25 +0800

    wip nl

--- explain.py
+++ explain.py
@@ -1,3 +1,5 @@
+from typing import List, Dict, Tuple
+
 import psycopg2
 
 DATABASE = "TPC-H"
@@ -11,14 +13,124 @@ conn = psycopg2.connect(database=DATABASE,
                         user=USER,
                         password=PASSWORD,
                         port=PORT)
+conn.autocommit = True
 cursor = conn.cursor()
 
-def get_query_plan(query: str):
-    cursor.execute("EXPLAIN (FORMAT JSON, VERBOSE) " + query)
+
+class QueryNode:
+    children: List = None
+    node_type: str = None
+    parallel_aware: str = None
+    startup_cost: float = None
+    total_cost: float = None
+    plan_rows: int = None
+    plan_width: int = None
+    output: List[str] = None
+    workers_planned: int = None
+    single_copy: bool = None
+
+    # extras for intermediate nodes
+    parent_relationship: str = None
+    join_type: str = None
+    inner_unique: bool = None
+    hash_cond: str = None
+
+    relation_name: str = None
+    schema: str = None
+    alias: str = None
+
+    filter: str = None
+
+    def __init__(self, explain_map):
+        self.node_type = explain_map.get("Node Type")
+        self.parallel_aware = explain_map.get("Parallel Aware")
+        self.startup_cost = explain_map.get("Startup Cost")
+        self.total_cost = explain_map.get("Total Cost")
+        self.plan_rows = explain_map.get("Plan Rows")
+        self.plan_width = explain_map.get("Plan Width")
+        self.output = explain_map.get("Output")
+        self.workers_planned = explain_map.get("Workers Planned")
+        self.single_copy = explain_map.get("Single Copy")
+        self.parent_relationship = explain_map.get("Parent Relationship")
+        self.join_type = explain_map.get("Join Type")
+        self.inner_unique = explain_map.get("Inner Unique")
+        self.hash_cond = explain_map.get("Hash Cond")
+        self.relation_name = explain_map.get("Relation Name")
+        self.schema = explain_map.get("Schema")
+        self.alias = explain_map.get("Alias")
+        self.filter = explain_map.get("Filter")
+
+        self.children = [QueryNode(p) for p in explain_map.get("Plans", [])]
+
+        self._explainMapping = {
+            "Gather": self._explain_gather,
+            "Hash Join": self._explain_hj,
+            "Seq Scan": self._explain_ss,
+            "Hash": self._explain_hash,
+        }
+
+    # In natural language, explain what this node does.
+    # We parse the explanation from bottom up.
+    def explain(self) -> List[Tuple[str, Dict[str, str]]]:
+        res = []
+        for i, child in enumerate(self.children):
+            res.extend(child.explain())
+            if i + 1 < len(self.children):
+                res.append(
+                    (
+                        f"Before we can process the {self.node_type} operation, we still have to process {len(self.children) - i - 1}"
+                        " more intermediate input, discussed immediately below.\n", None)
+                )
+
+        if self.node_type in self._explainMapping:
+            res.append(self._explainMapping[self.node_type]())
+        else:
+            print(self.node_type + " is not supported")
+            res.append(self._generic_explain())
+
+        return res
+
+    def _explain_gather(self) -> Tuple[str, Dict[str, str]]:
+        return f"A Gather operation is performed, combining the output of child nodes," \
+               " which are executed by parallel workers." \
+               "\nGather does not make any guarantee about ordering, unlike Gather Merge, " \
+               "which preserves sort order.\n", {"Test": ":123"}
+
+    def _explain_hj(self) -> Tuple[str, Dict[str, str]]:
+        return f"A hash join is performed. Hash join is an implementation of join in which one of the" \
+               " collections of rows to be joined is hashed on the join keys using a separate 'Hash' node. " \
+               "Postgres then iterates over the other collection of rows, for each one looking it up in the" \
+               " hash table to see if there are any rows it should be joined to.\n", {}
+
+    def _explain_ss(self) -> Tuple[str, Dict[str, str]]:
+        return "A sequential scan is performed. A Sequential Scan" \
+               " (or Seq Scan) reads the rows from the table, in order.\nWhen reading from a table, Seq Scans" \
+               " (unlike Index Scans) perform a single read operation (only the table is read).\n", {}
+
+    def _explain_hash(self) -> Tuple[str, Dict[str, str]]:
+        return f"A hash is performed, hashing the query rows for use by its parent operation, " \
+               "usually used to perform a JOIN.\n", {}
+
+    def _generic_explain(self) -> Tuple[str, Dict[str, str]]:
+        return f"A {self.node_type} operation is performed.\n", {}
+
+
+# returns the query plan graph node
+def get_query_plan(query: str) -> List[Tuple[str, Dict[str, str]]]:
+    # we do not commit the transaction so analyze does not change db state
+    cursor.execute("EXPLAIN (ANALYZE, COSTS, FORMAT JSON, VERBOSE, BUFFERS) " + query.rstrip(";") + ";")
     res = cursor.fetchone()
     if not res or not res[0]:
         print("no plan returned")
-        return None
+        return [("No plan returned", {})]
 
-    return res[0][0]
+    res = QueryNode(res[0][0]["Plan"]).explain()
+    for i, t in enumerate(res):
+        s, info_d = t
+        if i == len(res) - 1:
+            res[i] = f"{i + 1}. Finally, {s[0].lower()}{s[1:]}", info_d
+        else:
+            res[i] = f"{i + 1}. {s}", info_d
+
+    return res
     # print(res[0][0])
--- interface.py
+++ interface.py
@@ -1,10 +1,21 @@
 import dearpygui.dearpygui as dpg
+
 from explain import get_query_plan
 
 old_query_ref: int | str = None
 new_query_ref: int | str = None
 
-display_text_ref: int | str = None
+old_g: int | str = None
+new_g: int | str = None
+view_graphic_ref: int | str = None
+
+
+def view_graphic_callback(sender, app_data, user_data):
+    old_qep, new_qep = user_data
+    # place graphical visualization in a separate window pop up
+    with dpg.window(label="query diff", width=700, height=600, pos=(320, 70)):
+        dpg.add_text(str(old_qep) + "\n\n\n" + str(new_qep), wrap=550)
+
 
 def button_callback():
     if old_query_ref is None or new_query_ref is None:
@@ -15,19 +26,53 @@ def button_callback():
     # TODO: enclose this in a try and except and display errors back if any.
     old_qep = get_query_plan(old_q)
     new_qep = get_query_plan(new_q)
-    dpg.set_value(display_text_ref, str(old_qep) + "\n\n\n" + str(new_qep))
+
+    dpg.configure_item(view_graphic_ref, show=True)
+    dpg.set_item_user_data(view_graphic_ref, (old_qep, new_qep))
+
+    # place natural lang explanation in primary window (this will be scrollable)
+    dpg.add_text(f"Old Query Plan Explanation: \n\n", wrap=500, parent=old_g, color=[255, 255, 0])
+    for s, d in old_qep:
+        dpg.add_text(s + "\n", wrap=500, parent=old_g)
+        if not d:
+            continue
+        with dpg.collapsing_header(parent=old_g, label="Misc Info") as x:
+            t = dpg.add_table(label="Misc", parent=x, width=500, header_row=False)
+            dpg.add_table_column(parent=t)
+            dpg.add_table_column(parent=t)
+            for k, v in d.items():
+                with dpg.table_row(parent=t):
+                    dpg.add_text(k)
+                    dpg.add_text(v)
+
+    dpg.add_text(f"New Query Plan Explanation: \n\n", wrap=500, parent=new_g, color=[255, 255, 0])
+    for s, d in new_qep:
+        dpg.add_text(s + "\n", wrap=500, parent=new_g)
+        if not d:
+            continue
+
+        with dpg.collapsing_header(parent=new_g, label="Misc Info") as x:
+            t = dpg.add_table(label="Misc", parent=x, width=500, header_row=False)
+            dpg.add_table_column(parent=t)
+            dpg.add_table_column(parent=t)
+            for k, v in d.items():
+                with dpg.table_row(parent=t):
+                    dpg.add_text(k)
+                    dpg.add_text(v)
 
 
 def start():
     dpg.create_context()
-    dpg.create_viewport(title='Custom Title', width=1450)
+    dpg.create_viewport(title='Postgres SQL Query Plan Visualizer', width=1450)
     dpg.setup_dearpygui()
 
     with dpg.window(label="Example Window") as main_window:
+        dpg.set_primary_window(main_window, True)
+
         dpg.add_spacer(height=5)
         with dpg.group(horizontal=True):
-            dpg.add_spacer(width=550)
-            dpg.add_text("Postgres SQL Query Execution Plan Diff Visualizer")
+            dpg.add_spacer(width=510)
+            dpg.add_text("Postgres SQL Query Execution Plan Diff Visualizer", bullet=True, color=[255, 255, 0])
         dpg.add_spacer(height=30)
         with dpg.group(horizontal=True, horizontal_spacing=100):
             dpg.add_spacer(width=50)
@@ -45,7 +90,8 @@ def start():
                 dpg.add_text("New SQL Query")
                 global new_query_ref
                 new_query_ref = dpg.add_input_text(
-                    default_value="SELECT * FROM customer C, orders O WHERE C.c_custkey = O.o_custkey \nAND c.c_name LIKE '%cheng';",
+                    default_value="SELECT * FROM customer C, orders O WHERE C.c_custkey = O.o_custkey \n"
+                                  "AND c.c_name LIKE '%cheng';",
                     multiline=True,
                     width=500,
                     height=300,
@@ -60,11 +106,25 @@ def start():
 
         dpg.add_spacer(height=50)
         with dpg.group(horizontal=True):
-            dpg.add_spacer(width=300)
-            global display_text_ref
-            display_text_ref = dpg.add_text(wrap=900)
+            dpg.add_spacer(width=580)
+            global view_graphic_ref
+            view_graphic_ref = dpg.add_button(
+                label="View graphical visualization",
+                callback=view_graphic_callback,
+                show=False,
+            )
 
-        dpg.set_primary_window(main_window, True)  # Should fill viewport but does not on macOS
+        dpg.add_spacer(height=50)
+        with dpg.group(horizontal=True, horizontal_spacing=100):
+            dpg.add_spacer(width=50)
+            global old_g
+            with dpg.group(label="Old Query Explanation", width=600) as g:
+                old_g = g
+            global new_g
+            with dpg.group(label="New Query Explanation", width=600) as g:
+                new_g = g
+
+        dpg.add_spacer(height=100)
 
     dpg.show_viewport()
     dpg.start_dearpygui()
